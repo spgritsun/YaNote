ebcd949b0ca6ae9e2cbafa26dece1045a33e71a8
Sergei Gritsun <gritsun.sp@gmail.ru>
2025-10-27 18:33:45 +0200

    tests for test_routes.py are ready

--- notes/tests/test_routes.py
+++ notes/tests/test_routes.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from pytest_django.asserts import assertRedirects
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
@@ -24,18 +26,23 @@ class TestRoutes(TestCase):
         cls.slug_for_args = (cls.note.slug,)
 
     def test_pages_availability_for_anonymous_user(self):
-        url_status = {
+        name_status = {
             'notes:home': HTTPStatus.OK,
             'users:login': HTTPStatus.OK,
             'users:signup': HTTPStatus.OK,
             'users:logout': HTTPStatus.METHOD_NOT_ALLOWED,
         }
-
-        for name, expected_status in url_status.items():
+        for name, expected_status in name_status.items():
             with self.subTest(name=name):
                 url = reverse(name)
                 response = self.client.get(url)
                 self.assertEqual(response.status_code, expected_status)
+
+    def test_logout_for_post_method(self):
+        name = 'users:logout'
+        url = reverse(name)
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
 
     def test_pages_availability_for_auth_user(self):
         self.client.force_login(self.any_reg_user)
@@ -63,3 +70,21 @@ class TestRoutes(TestCase):
                     url = reverse(name, args=self.slug_for_args)
                     response = self.client.get(url)
                     self.assertEqual(response.status_code, status)
+
+    def test_redirects(self):
+        names = {
+            'notes:detail': self.slug_for_args,
+            'notes:edit': self.slug_for_args,
+            'notes:delete': self.slug_for_args,
+            'notes:add': None,
+            'notes:success': None,
+            'notes:list': None}
+        login_url = reverse('users:login')
+        for name, args in names.items():
+            if args is not None:
+                url = reverse(name, args=args)
+            else:
+                url = reverse(name)
+            expected_url = f'{login_url}?next={url}'
+            response = self.client.get(url)
+            assertRedirects(response, expected_url)
