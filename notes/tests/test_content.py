55aeffc76760223990af1ba35a6129b44f8b3177
Sergei Gritsun <gritsun.sp@gmail.ru>
2025-11-03 01:08:08 +0300

    test_logic.py is half ready

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -8,19 +8,21 @@ from .common import LIST_URL, ADD_URL, EDIT_URL
 class TestContent(TestSetUp):
 
     def test_notes_list_for_different_users(self):
-        users_status = {
-            self.author: True,
-            self.any_reg_user: False}
-        for user, status in users_status.items():
-            self.client.force_login(user)
-            response = self.client.get(LIST_URL)
-            object_list = response.context['object_list']
-            self.assertEqual(self.note in object_list, status)
+        users_status = (
+            (self.author, True),
+            (self.any_reg_user, False))
+        for user, status in users_status:
+            with self.subTest(user=user, status=status):
+                self._login_as(user)
+                response = self.client.get(LIST_URL)
+                object_list = response.context['object_list']
+                self.assertEqual(self.note in object_list, status)
 
     def test_pages_contains_form(self):
         urls = (ADD_URL, EDIT_URL)
-        self.client.force_login(self.author)
+        self._login_as(self.author)
         for url in urls:
-            response = self.client.get(url)
-            self.assertIn('form', response.context)
-            self.assertIsInstance(response.context['form'], NoteForm)
+            with self.subTest(user=self.author, url=url):
+                response = self.client.get(url)
+                self.assertIn('form', response.context)
+                self.assertIsInstance(response.context['form'], NoteForm)
--- notes/tests/test_logic.py
+++ notes/tests/test_logic.py
@@ -19,47 +19,48 @@ class TestLogic(TestSetUp):
         }
         cls.WARNING = WARNING
 
-    def setUp(self):
-        self.client.force_login(self.author)
-
     def test_user_can_create_note(self):
-
-        self.note.delete()
+        self._login_as(self.author)
+        notes_count = Note.objects.count()
         response = self.client.post(ADD_URL, data=self.form_data)
         self.assertRedirects(response, SUCCESS_URL)
-        self.assertEqual(Note.objects.count(), 1)
-        new_note = Note.objects.get()
+        self.assertEqual(Note.objects.count(), notes_count + 1)
+        new_note = Note.objects.get(slug=self.form_data['slug'])
         self.assertEqual(new_note.title, self.form_data['title'])
         self.assertEqual(new_note.text, self.form_data['text'])
         self.assertEqual(new_note.slug, self.form_data['slug'])
         self.assertEqual(new_note.author, self.author)
 
     def test_anonymous_user_cant_create_note(self):
-        self.client.logout()
-        self.note.delete()
+        self._login_as(None)
+        notes_count = Note.objects.count()
         response = self.client.post(ADD_URL, data=self.form_data)
         expected_url = f'{LOGIN_URL}?next={ADD_URL}'
         self.assertRedirects(response, expected_url)
-        self.assertEqual(Note.objects.count(), 0)
+        self.assertEqual(Note.objects.count(), notes_count)
 
     def test_not_unique_slug(self):
+        self._login_as(self.author)
+        notes_count = Note.objects.count()
         self.form_data['slug'] = self.note.slug
         response = self.client.post(ADD_URL, data=self.form_data)
         self.assertFormError(response.context['form'], 'slug',
                              self.note.slug + self.WARNING)
-        self.assertEqual(Note.objects.count(), 1)
+        self.assertEqual(Note.objects.count(), notes_count)
 
     def test_empty_slug(self):
-        self.note.delete()
+        self._login_as(self.author)
+        notes_count = Note.objects.count()
         self.form_data.pop('slug')
         response = self.client.post(ADD_URL, data=self.form_data)
         self.assertRedirects(response, SUCCESS_URL)
-        self.assertEqual(Note.objects.count(), 1)
-        new_note = Note.objects.get()
+        self.assertEqual(Note.objects.count(), notes_count + 1)
+        new_note = Note.objects.last()
         expected_slug = slugify(self.form_data['title'])
         self.assertEqual(new_note.slug, expected_slug)
 
     def test_author_can_edit_note(self):
+        self._login_as(self.author)
         response = self.client.post(EDIT_URL, self.form_data)
         self.assertRedirects(response, SUCCESS_URL)
         self.note.refresh_from_db()
@@ -68,8 +69,7 @@ class TestLogic(TestSetUp):
         self.assertEqual(self.note.slug, self.form_data['slug'])
 
     def test_other_user_cant_edit_note(self):
-        self.client.logout()
-        self.client.force_login(self.any_reg_user)
+        self._login_as(self.any_reg_user)
         response = self.client.post(EDIT_URL, self.form_data)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
         note_from_db = Note.objects.get(id=self.note.id)
@@ -78,13 +78,15 @@ class TestLogic(TestSetUp):
         self.assertEqual(self.note.slug, note_from_db.slug)
 
     def test_author_can_delete_note(self):
+        self._login_as(self.author)
+        notes_count = Note.objects.count()
         response = self.client.post(DELETE_URL)
         self.assertRedirects(response, SUCCESS_URL)
-        self.assertEqual(Note.objects.count(), 0)
+        self.assertEqual(Note.objects.count(), notes_count - 1)
 
     def test_other_user_cant_delete_note(self):
-        self.client.logout()
-        self.client.force_login(self.any_reg_user)
+        self._login_as(self.any_reg_user)
+        notes_count = Note.objects.count()
         response = self.client.post(DELETE_URL)
         self.assertEqual(response.status_code, HTTPStatus.NOT_FOUND)
-        self.assertEqual(Note.objects.count(), 1)
+        self.assertEqual(Note.objects.count(), notes_count)
